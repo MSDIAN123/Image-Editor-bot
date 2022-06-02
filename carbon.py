7e21a7b4adb92a5337c6291046cf7fd3147913d9
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-06-02 18:08:56 +0530

    Create carbon.py

---
+++ carbon.py
@@ -0,0 +1,25 @@
+from pyrogram import filters
+from io import BytesIO
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from szbot import sz, aiohttpsession as session
+from szbot.helpers.fsub import ForceSub
+from pyrogram.types import Message
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with session.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+def get_text(message: Message) -> [None, str]:
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
