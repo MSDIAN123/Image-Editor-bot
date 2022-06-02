f5a2b5a31b8a018de4848a56feefbed042090ecc
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-06-02 18:02:02 +0530

    Create caption.py

---
+++ caption.py
@@ -0,0 +1,20 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+imgcaption = """
+☘️ **Random Logo Created Successfully**✅
+◇───────────────◇
+🔥 **Created by** :  [🎨 Emo Logo Bot](https://t.me/Emo_Bot_Support)
+🌷 **Requestor** : {message.from_user.username}
+⚡️ **Powered By **: `【RC™】´
+◇───────────────◇
+
+"""
+repmark = InlineKeyboardMarkup(
+      [
+        [
+        InlineKeyboardButton(text="➕Add me to your group ➕", url=f"http://t.me/Emo_lOgo_Bot?startgroup=botstart") 
+        ],
+        
+        ]
+      ]      
+    )
